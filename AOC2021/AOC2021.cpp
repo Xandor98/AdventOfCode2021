314ae385b743b8165ae012583f2e46dcab40496e
Xandor98 <cevvoi1998@gmx.de>
2021-12-02 19:46:59 +0100

    Projektdateien hinzufügen.

---
+++ AOC2021/AOC2021.cpp
@@ -0,0 +1,12 @@
+﻿// AOC2021.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "AOC2021.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
